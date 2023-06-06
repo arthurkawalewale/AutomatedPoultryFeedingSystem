35ec93dcb4ef29fcd3baec72705f2d7939eb0667
Arthur Kawalewale <arthurkawalewale@gmail.com>
2023-06-06 12:48:50 +0200

    Now displaying data for the weekly metrics in line with each day even when no records are available for that day

--- automatedFeedingSystem/app/Http/Livewire/dashboard/WaterLevelStats.php
+++ automatedFeedingSystem/app/Http/Livewire/dashboard/WaterLevelStats.php
@@ -39,28 +39,32 @@ class WaterLevelStats extends ChartComponent
         $endDate = Carbon::now()->endOfWeek();
 
         // Retrieve records for the specified week
-        $water_level_data_sets = WaterReading::select(DB::raw('DAYNAME(created_at) AS day'), DB::raw('AVG(reservoir_reading) AS res_value, AVG(trough_reading) AS trough' ))
-            ->whereBetween('created_at', [$startDate, $endDate])
-            ->groupBy('day')
-            ->orderBy('created_at')
+        $water_level_data_sets = DB::table(DB::raw("(SELECT 'Sunday' AS day_of_week UNION ALL SELECT 'Monday' UNION ALL SELECT 'Tuesday' UNION ALL SELECT 'Wednesday' UNION ALL SELECT 'Thursday' UNION ALL SELECT 'Friday' UNION ALL SELECT 'Saturday') AS days_of_week"))
+            ->leftJoin(DB::raw("(SELECT DAYNAME(created_at) AS day_of_week, AVG(reservoir_reading) AS avg_reservoir, AVG(trough_reading) AS avg_trough FROM water_readings WHERE created_at BETWEEN '{$startDate}' AND '{$endDate}' GROUP BY day_of_week) AS averages"), 'days_of_week.day_of_week', '=', 'averages.day_of_week')
+            ->select('days_of_week.day_of_week', DB::raw('COALESCE(averages.avg_reservoir, 0) AS avg_reservoir'), DB::raw('COALESCE(averages.avg_trough, 0) AS avg_trough'))
             ->get();
 
+        // Convert the stdClass objects to WaterReading models
+        $water_level_data_sets = $water_level_data_sets->map(function ($item) {
+            $waterReading = new WaterReading();
+            $waterReading->day_of_week = $item->day_of_week;
+            $waterReading->avg_reservoir = $item->avg_reservoir;
+            $waterReading->avg_trough = $item->avg_trough;
+            return $waterReading;
+        });
+
         $labels = $water_level_data_sets->map(function(WaterReading $water_level_data_sets, $key) {
-            return $water_level_data_sets->day;
+            return $water_level_data_sets->day_of_week;
         });
 
         $datasets = new Collection([
             $water_level_data_sets->map(function(WaterReading $water_level_data_sets) {
-                return number_format($water_level_data_sets->res_value, 2, '.', '');
+                return number_format($water_level_data_sets->avg_reservoir, 2, '.', '');
             }),
 
             $water_level_data_sets->map(function(WaterReading $water_level_data_sets) {
-                return number_format($water_level_data_sets->trough, 2, '.', '');
+                return number_format($water_level_data_sets->avg_trough, 2, '.', '');
             }),
-        ]);
-
-        $labels = collect([
-            'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
         ]);
 
         return (new ChartComponentData($labels, $datasets));
